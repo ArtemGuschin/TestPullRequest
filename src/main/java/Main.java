994183d90279618c5bcd672ad88e9c1d5cdacd62
Artem <weter882@gmail.com>
2025-07-25 14:02:40 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World number  2 ");
+        System.out.println("Hello world pool request number 3 ");
     }
 }
